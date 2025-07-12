03a6bde8b4d09a95cdf0083abafec713d2115c9a
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-07-12 17:04:51 +0300

    Q1

--- Session 4/Program.cs
+++ Session 4/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1- Write a program that allows the user to insert an integer then print all numbers between 1 to that number
+            //Example
+            //Input: 5
+            //Output: 1, 2, 3, 4, 5
+
+            //Retry:
+            //Console.Write("Please enter a number: ");
+            //int Number;
+            //int.TryParse(Console.ReadLine(),out Number);
+            //if (Number <= 0)
+            //{
+            //    Console.WriteLine("Please enter a positive integer");
+            //    goto Retry;
+            //}
+            //else
+            //{
+            //    for (int i = 1; i <= Number; i++)
+            //    {
+            //        Console.Write(i);
+            //        if (i < Number)
+            //            Console.Write(", ");
+            //    }
+            //}
+                
+
+
+            #endregion
+
+
         }
     }
 }
