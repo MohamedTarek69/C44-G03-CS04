f82efd6dc4caabb15e7b0d3b594e6aa178cbb773
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-07-12 17:15:18 +0300

    Q3

--- Session 4/Program.cs
+++ Session 4/Program.cs
@@ -4,58 +4,89 @@
     {
         static void Main(string[] args)
         {
-            #region Q1- Write a program that allows the user to insert an integer then print all numbers between 1 to that number
-            //Example
-            //Input: 5
-            //Output: 1, 2, 3, 4, 5
+        #region Q1- Write a program that allows the user to insert an integer then print all numbers between 1 to that number
+        //Example
+        //Input: 5
+        //Output: 1, 2, 3, 4, 5
 
-            //Retry:
-            //Console.Write("Please enter a number: ");
-            //int Number;
-            //int.TryParse(Console.ReadLine(),out Number);
-            //if (Number <= 0)
-            //{
-            //    Console.WriteLine("Please enter a positive integer");
-            //    goto Retry;
-            //}
-            //else
-            //{
-            //    for (int i = 1; i <= Number; i++)
-            //    {
-            //        Console.Write(i);
-            //        if (i < Number)
-            //            Console.Write(", ");
-            //    }
-            //}
+        //Retry:
+        //Console.Write("Please enter a number: ");
+        //int Number;
+        //int.TryParse(Console.ReadLine(),out Number);
+        //if (Number <= 0)
+        //{
+        //    Console.WriteLine("Please enter a positive integer");
+        //    goto Retry;
+        //}
+        //else
+        //{
+        //    for (int i = 1; i <= Number; i++)
+        //    {
+        //        Console.Write(i);
+        //        if (i < Number)
+        //            Console.Write(", ");
+        //    }
+        //}
+
+        #endregion
+
+        #region Q2- Write a program that allows the user to insert an integer then print a multiplication table up to 12
+        // Example
+        // Input: 5
+        // Output: 5 10 15 20 25 30 35 40 45 50 55 60
+
+        //Retry:
+        //    Console.Write("Please enter a number: ");
+        //    int Number;
+        //    int.TryParse(Console.ReadLine(), out Number);
+        //    if (Number <= 0)
+        //    {
+        //        Console.WriteLine("Please enter a positive integer");
+        //        goto Retry;
+        //    }
+        //    else
+        //    {
+        //        for (int i = 1; i <= 12; i++)
+        //        {
+        //            Console.Write(i*Number);
+        //            if (i < 12)
+        //                Console.Write(" ");
+        //        }
+        //    }
+        #endregion
+
+        #region Q3- Write a program that allows the user to insert a number then print all even numbers between 1 to this number
+        // Example
+        // Input: 15
+        // Output: 2 4 6 8 10 12 14
+
+        //Retry:
+        //    Console.Write("Please enter a number: ");
+        //    int Number;
+        //    int.TryParse(Console.ReadLine(), out Number);
+        //    if (Number <= 0)
+        //    {
+        //        Console.WriteLine("Please enter a positive integer");
+        //        goto Retry;
+        //    }
+        //    else
+        //    {
+        //        for (int i = 0; i <= Number; i+=2)
+        //        {
+        //            if (i==0)
+        //            {
+        //                continue;
+        //            }
+        //            Console.Write(i);
+        //            if (i < Number)
+        //                Console.Write(" ");
+
+        //        }
+        //    }
 
             #endregion
 
-            #region Q2- Write a program that allows the user to insert an integer then print a multiplication table up to 12
-            // Example
-            // Input: 5
-            // Output: 5 10 15 20 25 30 35 40 45 50 55 60
 
-            //Retry:
-            //    Console.Write("Please enter a number: ");
-            //    int Number;
-            //    int.TryParse(Console.ReadLine(), out Number);
-            //    if (Number <= 0)
-            //    {
-            //        Console.WriteLine("Please enter a positive integer");
-            //        goto Retry;
-            //    }
-            //    else
-            //    {
-            //        for (int i = 1; i <= 12; i++)
-            //        {
-            //            Console.Write(i*Number);
-            //            if (i < 12)
-            //                Console.Write(" ");
-            //        }
-            //    }
-            #endregion
-
-            
         }
     }
 }
