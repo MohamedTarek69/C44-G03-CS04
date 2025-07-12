3b7d126030db1ac95ed7c3275b877baa8a964563
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-07-12 17:11:01 +0300

    Q2

--- Session 4/Program.cs
+++ Session 4/Program.cs
@@ -27,12 +27,35 @@
             //            Console.Write(", ");
             //    }
             //}
-                
-
 
             #endregion
 
+            #region Q2- Write a program that allows the user to insert an integer then print a multiplication table up to 12
+            // Example
+            // Input: 5
+            // Output: 5 10 15 20 25 30 35 40 45 50 55 60
 
+            //Retry:
+            //    Console.Write("Please enter a number: ");
+            //    int Number;
+            //    int.TryParse(Console.ReadLine(), out Number);
+            //    if (Number <= 0)
+            //    {
+            //        Console.WriteLine("Please enter a positive integer");
+            //        goto Retry;
+            //    }
+            //    else
+            //    {
+            //        for (int i = 1; i <= 12; i++)
+            //        {
+            //            Console.Write(i*Number);
+            //            if (i < 12)
+            //                Console.Write(" ");
+            //        }
+            //    }
+            #endregion
+
+            
         }
     }
 }
